70ce663dac3725b95d614aae94238cc58d2c1466
elihashemi <113266587+elihashemi@users.noreply.github.com>
2022-10-02 15:57:43 +0100

    inputComment

--- script.js
+++ script.js
@@ -66,3 +66,22 @@ test("There are at least 500 words on the page", () => {
 
 const console = document.getElementById("tests");
 prettify.toHTML(run(), console);*/
+function getNumberOrString(value) {
+  // Convert a string value to a number if possible
+  let number_value = Number(value);
+  if (Number.isNaN(number_value)) {
+    return value
+  } else {
+    return number_value
+  }
+}
+
+
+
+document.getElementById('button').addEventListener('click', (event) => {
+  let element_outputName = document.getElementById('outputName');
+  element_outputName.innerText = getNumberOrString(document.getElementById('inputName').value);
+  let element_outputText = document.getElementById('outputText');
+  element_outputText.innerText = getNumberOrString(document.getElementById('inputComment').value);
+
+});
